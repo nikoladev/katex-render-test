94eb0f244e8301843f2a24b39d44a3f846bb66ff
nikoladev <nikola@pilipovic.nl>
2019-09-07 14:43:15 -0300

    also render inline formulas

--- index.js
+++ index.js
@@ -12,39 +12,46 @@ const inputFilePath = path.join(__dirname, 'input.html')
 const outputFilePath = path.join(__dirname, 'output.html')
 
 const renderMath = function ({ input, output } = {}) {
+  // function for rendering Katex
+  const render = (str, ix) => {
+    // the even strings are the normal HTML parts
+    // which we don't need to render with Katex
+    if (ix % 2 === 0) {
+      return str
+    }
+
+    try {
+      // here we do the Katex rendering
+      return katex.renderToString(str, {
+        // This is an example of some macros that Elio used.
+        // It helps Katex resolve custom macros. If you do not
+        // have any macros then line 30 can just be this:
+        // return katex.renderToString(str)
+        macros: {
+          '\\iu': '\\mathrm{i}',
+          '\\eul': '\\mathrm{e}',
+          '\\dif': '\\mathrm{d}',
+          '\\cis': '\\operatorname{cis}',
+        },
+      })
+    } catch (err) {
+      // In case Katex had an error we color that part red
+      // and add the error message as an HTML comment
+      return `<!-- ${err} --> <span style='background-color: red; color:white; font-weight: bold;'>${str}</span>`
+    }
+  }
   // we read the original file
   const original = fs.readFileSync(input, 'utf8')
   // we split the original file by `$$` to get the parts that
   // are formulas and the parts that are not formulas
   const modified = original
+    // first render block formulas
     .split('$$')
-    .map((str, ix) => {
-      // the even strings are the normal HTML parts
-      // which we don't need to render with Katex
-      if (ix % 2 === 0) {
-        return str
-      }
-
-      try {
-        // here we do the Katex rendering
-        return katex.renderToString(str, {
-          // This is an example of some macros that Elio used.
-          // It helps Katex resolve custom macros. If you do not
-          // have any macros then line 30 can just be this:
-          // return katex.renderToString(str)
-          macros: {
-            '\\iu': '\\mathrm{i}',
-            '\\eul': '\\mathrm{e}',
-            '\\dif': '\\mathrm{d}',
-            '\\cis': '\\operatorname{cis}',
-          },
-        })
-      } catch (err) {
-        // In case Katex had an error we color that part red
-        // and add the error message as an HTML comment
-        return `<!-- ${err} --> <span style='background-color: red; color:white; font-weight: bold;'>${str}</span>`
-      }
-    })
+    .map(render)
+    .join('')
+    // then render inline formulas
+    .split('$')
+    .map(render)
     .join('')
   fs.writeFileSync(output, modified, 'utf8')
 }
